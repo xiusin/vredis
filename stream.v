ca875b932875367dc6f6d765e75f91e5569c81fe
xiusin <chenchengbin92@gmail.com>
2023-03-16 21:42:33 +0800

    redis

--- stream.v
+++ stream.v
@@ -1,26 +1,24 @@
 module vredis
 
 import os
-import time
-
 
 pub fn (mut r Redis) subscribe(channel string, channels... string) ! {
 	r.@lock()
 	defer {
 		r.unlock()
 	}
-
+	mut f := os.open_append('subscribe.log')!
+	defer {
+		f.close()
+	}
 	mut all_chans := [channel]
 	all_chans << channels
+	r.socket.write_string("SUBSCRIBE ${all_chans.join(' ')}")!
+	line :=  r.socket.read_line()
+	f.write_string(line + "\r\n")!
+}
 
-	// 起一个协程
-	spawn r.socket.write_string("SUBSCRIBE ${all_chans.join(' ')}")!
-
-	for {
-		os.write_file('pubsub.log', r.socket.read_line())!
-		time.sleep(time.minute)
-	}
-
-
-
+pub fn (mut r Redis) publish(channel string, message string) int {
+	res := r.send('PUBLISH ${channel} "${message}"') or { return 0 }
+	return res[1..].int()
 }
