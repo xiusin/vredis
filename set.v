d24c53fc0149752b702706e678dd9329ceb6587d
xiusin <chenchengbin92@gmail.com>
2023-08-12 22:03:54 +0800

    set

--- set.v
+++ set.v
@@ -1,26 +1,31 @@
 module vredis
 
 [params]
-pub struct SScanOpts {
+pub struct ScanOpts {
 	pattern string
 	count   i64
+	cursor u64
+}
+
+pub struct ScanReply {
+	cursor u64
+	result []string
 }
 
 pub fn (mut r Redis) sadd(key string, member1 string, member2 ...string) int {
 	mut members := [member1]
 	members << member2
 	members = members.map('"${it}"')
-	res := r.send('SADD "${key}" ${members.join(' ')}') or { ':0' }
-	return res.trim_left(':').int()
+
+	return r.send('SADD "${key}" ${members.join(' ')}') or { '0' }.int()
 }
 
 pub fn (mut r Redis) scard(key string) int {
-	res := r.send('SCARD "${key}"') or { ':0' }
-	return res.trim_left(':').int()
+	return r.send('SCARD "${key}"') or { '0' }.int()
 }
 
 pub fn (mut r Redis) sismember(key string, value string) bool {
-	return r.send('SISMEMBER "${key}" "${value}"') or { ':0' } == ':1'
+	return r.send('SISMEMBER "${key}" "${value}"') or { '0' } == '1'
 }
 
 pub fn (mut r Redis) spop(key string) string {
@@ -28,91 +33,83 @@ pub fn (mut r Redis) spop(key string) string {
 }
 
 pub fn (mut r Redis) smove(source string, destination string, member string) bool {
-	return r.send('SMOVE "${source}" "${destination}" "${member}"') or { ':0' } == ':1'
+	return r.send('SMOVE "${source}" "${destination}" "${member}"') or { '0' } == '1'
 }
 
-pub fn (mut r Redis) srandmember(key string, cnt ...int) []string {
+pub fn (mut r Redis) srandmember(key string, cnt ...int) ![]string {
 	count := if cnt.len > 0 { cnt[0] } else { 1 }
-	res := r.send('SRANDMEMBER "${key}" ${count}') or { return [] }
-	return res.split('\r\n')
+	ret := r.send('SRANDMEMBER "${key}" ${count}')!
+	return if ret.len > 0 {
+		ret.split('\r\n')
+	} else {
+		[]string{}
+	}
 }
 
 pub fn (mut r Redis) srem(key string, member1 string, member2 ...string) int {
 	mut members := [member1]
 	members << member2
 	members = members.map('"${it}"')
-	res := r.send('SREM "${key}" ${members.join(' ')}') or { ':0' }
-	return res.trim_left(':').int()
+	return r.send('SREM "${key}" ${members.join(' ')}') or { '0' }.int()
 }
 
-fn (mut r Redis) multi_keys_handle(cmd string, key string, keys []string) []string {
+fn (mut r Redis) multi_keys_handle(cmd string, key string, keys []string) ![]string {
 	mut all_keys := [key]
 	all_keys << keys
 	all_keys = all_keys.map('"${it}"')
-	res := r.send('${cmd} ${all_keys.join(' ')}') or { return [] }
-	return res.split('\r\n')
+	return r.send('${cmd} ${all_keys.join(' ')}')!.split('\r\n')
 }
 
 fn (mut r Redis) multi_keys_store_handle(cmd string, key string, keys []string) int {
 	mut all_keys := [key]
 	all_keys << keys
 	all_keys = all_keys.map('"${it}"')
-	res := r.send('${cmd} ${all_keys.join(' ')}') or { ':0' }
-	return res.trim_left(':').int()
+	return r.send('${cmd} ${all_keys.join(' ')}') or { '0' }.int()
 }
 
-pub fn (mut r Redis) sunion(key string, keys ...string) []string {
-	return r.multi_keys_handle('SUNION', key, keys)
+pub fn (mut r Redis) sunion(key string, keys ...string) ![]string {
+	return r.multi_keys_handle('SUNION', key, keys)!
 }
 
 pub fn (mut r Redis) sunionstore(key string, keys ...string) int {
 	return r.multi_keys_store_handle('SUNIONSTORE', key, keys)
 }
 
-pub fn (mut r Redis) sdiff(key string, keys ...string) []string {
-	return r.multi_keys_handle('SDIFF', key, keys)
+pub fn (mut r Redis) sdiff(key string, keys ...string) ![]string {
+	return r.multi_keys_handle('SDIFF', key, keys)!
 }
 
 pub fn (mut r Redis) sdiffstore(key string, keys ...string) int {
 	return r.multi_keys_store_handle('SDIFFSTORE', key, keys)
 }
 
-pub fn (mut r Redis) sinter(key string, keys ...string) []string {
-	return r.multi_keys_handle('SINTER', key, keys)
+pub fn (mut r Redis) sinter(key string, keys ...string) ![]string {
+	return r.multi_keys_handle('SINTER', key, keys)!
 }
 
-// pub fn (mut r Redis) sinterstore(key string, keys ...string) int {
-// 	return r.multi_keys_store_handle('SINTERSTORE', key, keys)
-// }
-
-pub struct ScanResult {
-	cursor u64
-	result []string
+pub fn (mut r Redis) smembers(key string) ![]string {
+	return r.send('SMEMBERS "${key}"')!.split("\r\n")
 }
 
-pub fn (mut r Redis) sscan(key string, cursor i64, opts SScanOpts) !ScanResult {
-	mut cmd := 'SSCAN "${key}" ${cursor} '
+pub fn (mut r Redis) sinterstore(key string, keys ...string) int {
+	return r.multi_keys_store_handle('SINTERSTORE', key, keys)
+}
+
+pub fn (mut r Redis) sscan(key string, opts ScanOpts) !ScanReply {
+	mut cmd := 'SSCAN "${key}" ${opts.cursor} '
+
 	if opts.pattern.len > 0 {
 		cmd += 'MATCH "${opts.pattern}" '
 	}
 	if opts.count > 0 {
 		cmd += 'COUNT ${opts.count}'
 	}
-	r.@lock()
-	defer {
-		r.unlock()
+
+	next_cursor, members := r.send(cmd)!.split_once("\r\n") or {
+		return error('parse reply content failed')
 	}
-	next_cursor := r.send(cmd)!
-	mut res := []string{cap: if opts.count > 0 { int(opts.count) } else { 10 }}
-	for {
-		line := r.read_reply()!
-		if line.len == 0 {
-			break
-		}
-		res << line[0..line.len - 2]
-	}
-	return ScanResult{
+	return ScanReply{
 		cursor: next_cursor.u64()
-		result: res
+		result: members.split("\r\n")
 	}
 }
--- set_test.v
+++ set_test.v
@@ -1,27 +1,37 @@
 module vredis
 
+
 fn test_set()! {
-	// mut redis_scribe := new_client(host: '124.222.103.232', port: 6379, requirepass: 'yuAU702G!!')!
-	// redis_scribe.subscribe('messages')!
-	//
-	// println('end')
-	// for i in 0 .. 1000 {
-	// 	redis.sadd('sets', 'v${i + 30}')
-	// }
-	//
-	// println(redis.sadd('sets2', 'v21', 'v22', 'v30'))
-	//
-	// println('scard = ${redis.scard('sets')}')
-	// println('sdiff = ${redis.sdiff('sets', 'sets2')}')
-	// println('sunion = ${redis.sunion('sets', 'sets2')}')
-	// println('sinter = ${redis.sinter('sets', 'sets2')}')
-	// println('sinterstore = ${redis.sinterstore('sinterstore', 'sets', 'sets2')}')
-	// println('sismember = ${redis.sismember('sets', 'v2')}')
-	// println('sismember = ${redis.sismember('sets', 'v5')}')
-	// println(redis.sscan('sets', 0)!)
+	mut redis := new_client()!
+	defer {
+		redis.close() or {}
+	}
 
+	assert redis.flushall()!
+	assert redis.sadd('sets', 'v11', 'v12', 'v_') == 3
+	assert redis.sadd('sets2', 'v21', 'v22', 'v_') == 3
+	assert redis.scard('sets') == 3
+	assert redis.sdiff('sets', 'sets2')!.str() == "['v12', 'v11']"
+	assert redis.sdiff('sets2', 'sets')!.str() == "['v22', 'v21']"
+	assert redis.sunion('sets', 'sets2')!.len == 5
+	assert redis.sinter('sets', 'sets2')!.str() == "['v_']"
+	assert redis.sismember('sets', 'v12')
+	assert redis.sismember('sets', 'v6') == false
+	// assert redis.spop('sets').starts_with('v')
+	// assert redis.sadd('sets', 'v_') >= 0
+	assert redis.smove('sets', 'set2', 'v_')
+	assert redis.sismember('sets', 'v_') == false
+	assert redis.srandmember('set', 2)!.len == 0
+	assert redis.srandmember('sets', 3)!.len == 2
+	redis.sadd('sets', 'v111')
+	assert redis.srem('sets', 'v111') > 0
+	redis.sadd('sets', 'v_')
+	assert redis.sinterstore('vredis_sinterstore', 'sets', 'sets2') == 1
+	assert redis.smembers('vredis_sinterstore')!.str() == "['v_']"
 
+	for i := 0; i<100; i++ {
+		redis.sadd('vredis_sccan', 'V${i}')
+	}
 
-
-
+	println(redis.sscan('vredis_sccan', count: 10)!)
 }
