a7f9c855c9df349ec594e136d72c725370aaf628
xiusin <chenchengbin92@gmail.com>
2023-08-14 18:31:52 +0800

    add test

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 VRedis is a Redis client service written in the V programming language. It allows you to connect to a Redis database over the network, send commands, and receive responses. Please note that this project is currently under **development** and is not recommended for use in production projects.
 
+> Almost support all Redis commands.
+
 # Installation
 
 Before using VRedis, you will need to install the V programming language compiler. Please visit the [V language website](https://vlang.io) for installation instructions. Once you have installed the V compiler, you can install VRedis using the following command:
@@ -22,17 +24,23 @@ import xiusin.vredis
 Once you have successfully connected, you can send commands using the `send()` method and receive responses using the `recv()` method:
 
 ```vlang
-client := vredis.new_client()
-client.connect("127.0.0.1", 6379)
-client.send("SET", "mykey", "hello")
-res := client.recv()
-println(res) // outputs "OK"
+redis := vredis.new_client(host: '127.0.0.1', port: 6379, db: 1, name: 'vclient', requirepass: '')
+
+redis.set("mykey", "hello")!
+redis.keys('*')!
+redis.del('mykey')!
+redis.hset('website', 'api', 'api.vlang.io')!
+redis.zadd('sets', 1, 'v1', '2', 'v2')!
+
+// For more usage, please refer to the test cases.
+
+redis.flushall()!
 ```
 
 Finally, when you are finished with the connection, use the close() method to close it:
 
 ```vlang
-client.close()
+redis.close()
 ```
 
 # Contributing
--- hash_test.v
+++ hash_test.v
@@ -8,21 +8,21 @@ fn test_hash()! {
 
 	assert redis.flushall()!
 
-	assert redis.hset('website', 'api', 'api.vlang.io')
-	assert redis.hset('website', 'www', 'www.vlang.io')
-	assert redis.hset('website', 'vpm', 'vpm.vlang.io')
+	assert redis.hset('website', 'api', 'api.vlang.io')!
+	assert redis.hset('website', 'www', 'www.vlang.io')!
+	assert redis.hset('website', 'vpm', 'vpm.vlang.io')!
 	assert redis.hlen('website')! == 3
-	assert redis.hexists('website', 'api')
-	assert redis.hexists('website', 'vpc') == false
+	assert redis.hexists('website', 'api')!
+	assert redis.hexists('website', 'vpc')! == false
 	assert redis.hkeys('website')!.str() == "['api', 'www', 'vpm']"
 	assert redis.hget('website', 'api')! == 'api.vlang.io'
 	assert redis.hgetall('website')!.str() == "{'api': 'api.vlang.io', 'www': 'www.vlang.io', 'vpm': 'vpm.vlang.io'}"
-	assert redis.hdel('website', 'api', 'doc')
-	assert redis.hdel('website', 'api', 'doc') == false
+	assert redis.hdel('website', 'api', 'doc')!
+	assert redis.hdel('website', 'api', 'doc')! == false
 	assert redis.hincrby('website', 'counter', 1)! == 1
 	assert redis.hincrbyfloat('website', 'counter', 1.1)! == 2.1
-	assert redis.hsetnx('website', 'doc', 'doc.vlang.io') == true
-	assert redis.hsetnx('website', 'doc', 'doc.vlang.io') == false
+	assert redis.hsetnx('website', 'doc', 'doc.vlang.io')! == true
+	assert redis.hsetnx('website', 'doc', 'doc.vlang.io')! == false
 	assert redis.hvals('website')!.len == 4
 	assert redis.hmget('website', 'doc', 'api')!.len == 2
 	assert redis.hmset('website', 'v1', 'v1.vlang.io', 'v2', 'v2.vlang.io')!
